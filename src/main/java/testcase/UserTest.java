b0afc8efe8f49bf705fc0530cf53dd548c7383c8
Suraj Thapaliya <opensource.suraj@gmail.com>
2016-08-17 23:29:34 -0500

    Test Case

---
+++ src/main/java/testcase/UserTest.java
@@ -0,0 +1,18 @@
+package testcase;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+	private SessionFactory sessionFactory;
+    private Session session;
+    
+
+    public void before() {
+    	
+    }
+    
+}
